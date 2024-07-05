77106b3c22966393126b10bad4818da3380a2f37
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-07-05 18:58:40 +0500

    Add SpecialPremiumContentAuthorizationHandler class to used to check whether the requirement is satisfied. If the user has a Premium subscription and is based in the country where the premium content can be accessed, the requirement is satisfied

--- AuthApp2/Authorization/PolicyBasedAuthorization/Requirements/SpecialPremiumContentRequirement.cs
+++ AuthApp2/Authorization/PolicyBasedAuthorization/Requirements/SpecialPremiumContentRequirement.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 
-namespace AuthApp2.Authorization.PolicyBasedAuthorization;
+namespace AuthApp2.Authorization.PolicyBasedAuthorization.Requirements;
 
 public class SpecialPremiumContentRequirement : IAuthorizationRequirement
 {
     public string Country { get; }
     public SpecialPremiumContentRequirement(string country)
     {
-        Country = country;  
+        Country = country;
     }
 }
---
+++ AuthApp2/Authorization/PolicyBasedAuthorization/SpecialPremiumContentAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using AuthApp2.Authorization.PolicyBasedAuthorization.Requirements;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace AuthApp2.Authorization.PolicyBasedAuthorization;
+
+public class SpecialPremiumContentAuthorizationHandler : AuthorizationHandler<SpecialPremiumContentRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, SpecialPremiumContentRequirement requirement)
+    {
+        var hasPremiumSubscriptionClaim = context.User.HasClaim(claim => claim.Type == "Subscription" &&  claim.Value == "Premium");
+
+        if(!hasPremiumSubscriptionClaim)
+        {
+            return Task.CompletedTask;
+        }
+        
+        var countryClaim = context.User.FindFirst(claim => claim.Type == System.Security.Claims.ClaimTypes.Country);
+        
+        if (countryClaim == null || string.IsNullOrWhiteSpace(countryClaim.ToString())) 
+        {
+            return Task.CompletedTask;
+        }
+
+        if(countryClaim.Value == requirement.Country)
+        {
+            context.Succeed(requirement);
+        }
+
+        return Task.CompletedTask;
+    }
+}
